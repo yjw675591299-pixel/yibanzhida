319bfadbcdcaa620a65e42667940c2e5f209154f
esta <2659610803@qq.com>
2026-05-22 16:20:54 +0800

    建立Maven和Spring Boot启动类

---
+++ backend/src/main/java/com/atzhiban/java/ai/langchain4j/ZhiDaApp.java
@@ -0,0 +1,10 @@
+﻿package com.atzhida.java.ai.langchain4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ZhiDaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ZhiDaApp.class, args);
+    }
+}
+
